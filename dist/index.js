7de833c9a198e5f3f17e4061c8d116f30c363a80
Alexander Patrick <alexander.patrick@milkbox.com>
2021-08-31 14:21:21 -0400

    fix: Make cliq host dynamic
    since the region may not always be eu

--- dist/index.js
+++ dist/index.js
@@ -13663,16 +13663,17 @@ async function run() {
       throw new Error('Please set [SLACK_WEBHOOK_URL] environment variable');
     }
 
-    const cliqToken = process.env.CLIQ_WEBHOOK_TOKEN;
-
-    if (platforms.includes('cliq') && !cliqToken) {
-      throw new Error('Please set [CLIQ_WEBHOOK_TOKEN] environment variable');
-    }
-
+    const cliqHost = process.env.CLIQ_HOST || 'cliq.zoho.eu';
     const cliqChannel = process.env.CLIQ_CHANNEL;
 
     if (platforms.includes('cliq') && !cliqChannel) {
       throw new Error('Please set [CLIQ_CHANNEL] environment variable');
+    }
+
+    const cliqToken = process.env.CLIQ_WEBHOOK_TOKEN;
+
+    if (platforms.includes('cliq') && !cliqToken) {
+      throw new Error('Please set [CLIQ_WEBHOOK_TOKEN] environment variable');
     }
 
     let jobStatus = core$1.getInput('status');
@@ -13695,7 +13696,7 @@ async function run() {
       if (platform === 'slack') {
         url = slackUrl;
       } else if (platform === 'cliq') {
-        url = `https://cliq.zoho.eu/api/v2/channelsbyname/${cliqChannel}/message?zapikey=${cliqToken}`;
+        url = `https://${cliqHost}/api/v2/channelsbyname/${cliqChannel}/message?zapikey=${cliqToken}`;
       }
 
       await notify(jobStatus, url, platform);
--- src/index.ts
+++ src/index.ts
@@ -19,14 +19,16 @@ async function run() {
       throw new Error('Please set [SLACK_WEBHOOK_URL] environment variable');
     }
 
-    const cliqToken = process.env.CLIQ_WEBHOOK_TOKEN;
-    if (platforms.includes('cliq') && !cliqToken) {
-      throw new Error('Please set [CLIQ_WEBHOOK_TOKEN] environment variable');
-    }
+    const cliqHost = process.env.CLIQ_HOST || 'cliq.zoho.eu';
 
     const cliqChannel = process.env.CLIQ_CHANNEL;
     if (platforms.includes('cliq') && !cliqChannel) {
       throw new Error('Please set [CLIQ_CHANNEL] environment variable');
+    }
+
+    const cliqToken = process.env.CLIQ_WEBHOOK_TOKEN;
+    if (platforms.includes('cliq') && !cliqToken) {
+      throw new Error('Please set [CLIQ_WEBHOOK_TOKEN] environment variable');
     }
 
     let jobStatus = core.getInput('status');
@@ -49,7 +51,7 @@ async function run() {
         if (platform === 'slack') {
           url = slackUrl;
         } else if (platform === 'cliq') {
-          url = `https://cliq.zoho.eu/api/v2/channelsbyname/${cliqChannel}/message?zapikey=${cliqToken}`;
+          url = `https://${cliqHost}/api/v2/channelsbyname/${cliqChannel}/message?zapikey=${cliqToken}`;
         }
         await notify(jobStatus as JobStatus, url, platform);
       })
